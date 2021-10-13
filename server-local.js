c4883ae8afb565b8ea336577f0aebb1765df53dc
onbookdbs <92363442+onbookdbs@users.noreply.github.com>
2021-10-13 10:32:53 +0700

    Update server-local.js

--- server-local.js
+++ server-local.js
@@ -8,8 +8,19 @@ app.set('view engine', 'pug');
 
 // Local request handlers.
 app.get('/', (req, res) => {
-  res.render('index');
-  res.send('Hello World!')  
+    let hostName = req.headers['x-forwarded-proto'] + "://" + req.headers.host;
+    res.header('Content-Type', 'application/xml');
+    let url = "https://db.blitarkab.go.id/id/";
+    let settings = { method: "Get" };
+    fetch(url, settings)
+    .then(res => res.json())
+    .then((json) => {
+        res.write('<?xml version="1.0" encoding="UTF-8"?><?xml-stylesheet type="text/xsl" href="'+ hostName + '/main-sitemap.xsl"?>\n  <urlset xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:image="http://www.google.com/schemas/sitemap-image/1.1" xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd http://www.google.com/schemas/sitemap-image/1.1 http://www.google.com/schemas/sitemap-image/1.1/sitemap-image.xsd" xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">\n');
+        json.forEach(function(json) {
+            res.write(" <url>\n"), res.write("   <loc>" + hostName+'/?id=A4999'+json + "</loc>\n"), res.write("   <lastmod>" + (new Date).toISOString() + "</lastmod>\n"), res.write(" </url>\n")
+        }),
+        res.write("</urlset>"), res.end();
+    });  
 });
 
 // Route Files.
